d5d8c289fe7b761a5dda85ddf7d00801d7dc358f
Alamgir Khan <k.alamgir2002@gmail.com>
2022-12-05 06:27:35 -0500

    bug fixing img

--- main.js
+++ main.js
@@ -266,7 +266,7 @@ async function JoinConn(conn, code_id) {
 function addUserImg() {
   const usersDiv = document.getElementById('userImages')
   const userImg = document.createElement('img')
-  userImg.src = './user.png'
+  userImg.src = 'https://direc-tl-y.vercel.app/user.png'
   usersDiv.appendChild(userImg)
 }
 
